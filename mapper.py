d32b73e776a45892be649d6509f34a95c09406ee
salu2303 <saloni230301@gmail.com>
2022-11-03 18:11:00 -0400

    commit 2

--- mapper.py
+++ mapper.py
@@ -10,32 +10,13 @@ def getairline_namebyid(input):
     return rows
 
 def add_airline(input):
-    cursor,sqliteConnection=connect()    
-    add = f'INSERT INTO airline(airline_id,callsign,date_founded,iata_code,airline_name,country_name,status) VALUES("{input.airline_id}","{input.callsign}","{input.date_founded}","{input.iata_code}","{input.airline_name}","{input.country_name}","{input.status}")'
-    rows = cursor.execute(add)
-    sqliteConnection.commit()
-    print("Added")
-
+    pass
 def getallairline():
-    cursor,sqliteConnection=connect()    
-    get_all = 'SELECT * FROM airline'
-    rows = cursor.execute(get_all).fetchall()
-    return rows
-
+    pass
 def update_airline(input):
-    cursor,sqliteConnection=connect()    
-    update = f'UPDATE airline SET callsign="{input.callsign}",date_founded="{input.date_founded}",iata_code="{input.iata_code}",airline_name="{input.airline_name}",country_name="{input.country_name}",status="{input.status}" WHERE airline_id="{input.airline_id}"'
-    rows = cursor.execute(update)
-    sqliteConnection.commit()
-    print("Updated")
-
+    pass
 def delete_airline(input):
-    cursor,sqliteConnection=connect()    
-    delete = 'DELETE FROM airline WHERE airline_id="'+input+'"'
-    rows = cursor.execute(delete)
-    sqliteConnection.commit()
-    print("Deleted")
-
+    pass
 
 
    
